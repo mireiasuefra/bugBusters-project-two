425ed3e7cc003e4609ae9b530ea2bcc733eaf9c7
mireiasuefra <mireia.suefra@gmail.com>
2024-03-04 16:16:31 +0100

    commit inicial

---
+++ LucasTeam-V2/src/main/java/BugsBusters/LucasTeamV2Application.java
@@ -0,0 +1,13 @@
+package BugsBusters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasTeamV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasTeamV2Application.class, args);
+	}
+
+}
